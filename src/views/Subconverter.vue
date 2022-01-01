f9838b0051717d84629a044a0564fd976c82cceb
Peter Lee <52237591+GodApex@users.noreply.github.com>
2022-01-01 13:22:45 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -254,7 +254,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "sub.godapex.com": "https://sub.godapex.com/sub?",
+          "sub.godapex.com(本站提供)": "https://sub.godapex.com/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
